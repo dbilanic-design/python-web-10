e5690ed743500cef40f1f08f957d49684d2ec72a
dbilanic-design <dbilanic@gmail.com>
2026-04-29 15:50:26 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "quotes_db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
